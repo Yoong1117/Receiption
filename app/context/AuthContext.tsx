66bc1a61422cd1472fd72e18e69e7f99a2123e98
jiangyoong <jiang.yoong@sorable.com>
2025-08-18 22:12:11 +0800

    Fix google sign in

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -87,7 +87,7 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`,
+        redirectTo: `${process.env.VITE_APP_URL}/dashboard`,
       },
     });
 
